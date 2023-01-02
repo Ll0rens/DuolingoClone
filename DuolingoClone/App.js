4c7f20b96ef9f21dffc7603d56bae915cce3dc85
ll0rens <llorens.martinez.marcos@gmail.com>
2023-01-02 17:33:00 +0100

    Refactor ImageOption component

--- DuolingoClone/App.js
+++ DuolingoClone/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import { Text, View, Image } from "react-native";
 import styles from "./App.styles"
 import ImageOption from "./src/components/ImageOption";
+import question from "./assets/data/oneQuestionWithOption"
 
 
 const App = () => {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}> Which of these is in the "glass"</Text>
+      <Text style={styles.title}>{question.question}</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        {question.options.map((option) => (
+          <ImageOption
+            key={option.id}
+            image={option.image}
+            text={option.text}
+          />
+        ))}
       </View>
     </View>
   )
--- DuolingoClone/src/components/ImageOption/ImageOption.js
+++ DuolingoClone/src/components/ImageOption/ImageOption.js
@@ -2,19 +2,17 @@ import React from "react"
 import {View, Image, Text } from 'react-native'
 import styles from './styles'
 
-const ImageOption = () => {
-    return (
-      <View style={styles.optionContainer}>
-        <Image
-            source={{
-            uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png"
-            }}
-            style={styles.optionImage}
-            resizeMode="contain"
-            />
-        <Text style={styles.optionText}>Glass</Text>
-    </View>
-    )
-  }
+const ImageOption = ({image, text}) => (
+  <View style={styles.optionContainer}>
+    <Image
+        source={{
+        uri: image
+        }}
+        style={styles.optionImage}
+        resizeMode="contain"
+        />
+    <Text style={styles.optionText}>{text}</Text>
+  </View>
+);
 
   export default ImageOption;
