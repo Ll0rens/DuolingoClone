808374525d36e532b422ba71cf8162b18b294d8c
ll0rens <llorens.martinez.marcos@gmail.com>
2023-01-03 04:25:41 +0100

    Button component

--- DuolingoClone/App.js
+++ DuolingoClone/App.js
@@ -3,10 +3,13 @@ import { Text, View, Image } from "react-native";
 import styles from "./App.styles"
 import ImageOption from "./src/components/ImageOption";
 import question from "./assets/data/oneQuestionWithOption"
-
+import Button from "./src/components/Button";
 
 const App = () => {
   const [selected, setSelected] = useState(null);
+  const onButtonPress = () => {
+    console.warn("Pressed");
+  }
   return (
     <View style={styles.root}>
       <Text style={styles.title}>{question.question}</Text>
@@ -21,6 +24,7 @@ const App = () => {
           />
         ))}
       </View>
+      <Button text="Check" onPress={onButtonPress} disabled={!selected}/>
     </View>
   )
 };
